7290c9bc9cf6dd088bf856061aca7b1faa4fbbba
Koichi Higashi <khigashi@bio.titech.ac.jp>
2014-12-08 20:57:08 +0900

    Create write.h

---
+++ write.h
@@ -0,0 +1,10 @@
+/*
+ * write.h
+ */
+#ifndef GAMMAGLM_WRITE_H
+#define GAMMAGLM_WRITE_H
+#include <stdio.h>
+
+extern void save_model(char *model, double *wa, double *wb, int n_features);
+
+#endif
