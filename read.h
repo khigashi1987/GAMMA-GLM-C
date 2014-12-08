393ba0396181a8e6daf22289c51be00d68477cc2
Koichi Higashi <khigashi@bio.titech.ac.jp>
2014-12-08 20:56:24 +0900

    Create read.h

---
+++ read.h
@@ -0,0 +1,10 @@
+/*
+ * read.h
+ */
+#ifndef GAMMAGLM_READ_H
+#define GAMMAGLM_READ_H
+#include <stdio.h>
+
+extern void read_data(char *train, double **y, double ***features, int *n_samples, int *n_features);
+
+#endif
