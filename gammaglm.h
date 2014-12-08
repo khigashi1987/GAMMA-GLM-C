0d37b71df7d8cadfb21e57b0f98efe787f356ee2
Koichi Higashi <khigashi@bio.titech.ac.jp>
2014-12-08 20:53:01 +0900

    Create gammaglm.h

---
+++ gammaglm.h
@@ -0,0 +1,16 @@
+/*
+ * gammaglm.h
+ * header file for gammaglm
+ */
+#ifndef GAMMAGLM_GAMMAGLM_H
+#define GAMMAGLM_GAMMAGLM_H
+#include <stdlib.h>
+#include "read.h"
+
+#define ITER_DEFAULT 1000
+#define SIGMA_DEFAULT 0.1
+#define EPS_DEFAULT 0.2
+
+void usage(void);
+
+#endif
